9cb8fc556bae44b4a4acb4c2c93b1753c7fd69cb
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-17 04:09:25 +0700

    refactor: add icon component and logic

---
+++ src/icons/config.tsx
@@ -0,0 +1,58 @@
+export const ICON_COLLECTION: { [key: string]: any } = {
+  dotmenu: () => {
+    return (
+      <>
+        <circle cx="12" cy="12" r="1"></circle>
+        <circle cx="12" cy="5" r="1"></circle>
+        <circle cx="12" cy="19" r="1"></circle>
+      </>
+    );
+  },
+  trash: () => {
+    return (
+      <>
+        <polyline points="3 6 5 6 21 6"></polyline>
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+        <line x1="10" y1="11" x2="10" y2="17"></line>
+        <line x1="14" y1="11" x2="14" y2="17"></line>
+      </>
+    );
+  },
+  send: () => {
+    return (
+      <>
+        <circle cx="12" cy="12" r="10" />
+        <path d="M16 12l-4-4-4 4M12 16V9" />
+      </>
+    );
+  },
+  refresh: () => {
+    return (
+      <>
+        <path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path>
+      </>
+    );
+  },
+  copy: () => {
+    return (
+      <>
+        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+      </>
+    );
+  },
+  like: () => {
+    return (
+      <>
+        <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
+      </>
+    );
+  },
+  dislike: () => {
+    return (
+      <>
+        <path d="M21.5 2v6h-6M21.34 15.57a10 10 0 1 1-.57-8.38" />
+      </>
+    );
+  },
+};
---
+++ src/icons/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { ICON_COLLECTION } from "./config";
+
+export type IconNameType =
+  | "dotmenu"
+  | "like"
+  | "dislike"
+  | "copy"
+  | "refresh"
+  | "send"
+  | "trash";
+
+interface IconProp {
+  width?: number;
+  height?: number;
+  stroke?: string;
+  iconName: IconNameType;
+}
+
+const Icon: React.FC<IconProp> = ({
+  width = "24",
+  height = "24",
+  stroke = "#000000",
+  iconName,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={stroke}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      {ICON_COLLECTION[iconName]()}
+    </svg>
+  );
+};
+
+export default Icon;
