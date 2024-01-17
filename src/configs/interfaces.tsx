fe794de97d3c514d6f60fb9d211cfa3f29c2d4bd
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-17 11:47:32 +0700

    refactor: add interfaces chats

--- src/configs/interfaces.tsx
+++ src/configs/interfaces.tsx
@@ -1,10 +1,15 @@
 export interface Chat {
+  ID: string;
   message: string;
   date: string;
   from: string;
+  liked?: boolean;
+  disliked?: boolean;
 }
 
 export interface ChatBubble {
   isActiveDelete: boolean;
+  onClickCheck: (ID: string) => void;
+  deleteCollection?: String[];
   chat: Chat;
 }
