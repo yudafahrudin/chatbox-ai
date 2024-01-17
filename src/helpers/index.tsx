1e82dac45e290d685e8ec947424e7c3cbc92b549
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-17 11:47:15 +0700

    refactor: add helper for utility

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,1 +1,15 @@
+import { format } from "date-fns";
+import { Chat } from "@/configs/interfaces";
+
 export const copyText = (text: string) => navigator.clipboard.writeText(text);
+
+export const saveLocalMessage = (chat: Chat[]) => {
+  localStorage.setItem("message", JSON.stringify(chat));
+};
+export const loadLocalMessage = () => {
+  return JSON.parse(localStorage.getItem("message") || "[]");
+};
+
+export const formatDateTime = (date: string) => {
+  return format(date, "hh:mm aaaaa'm'");
+};
