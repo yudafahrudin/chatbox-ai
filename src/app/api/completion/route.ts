43c3e54caff44a0a46a8ad1f14b97ddd01874dfc
yudafahrudin <ahmadyudafahrudin@gmail.com>
2024-01-17 12:35:02 +0700

    refactor: configure deloy project

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -3,7 +3,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
  
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: "",
 });
  
 // IMPORTANT! Set the runtime to edge
